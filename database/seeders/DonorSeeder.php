58c1d421aad9adb77d8ec048e03faecb87b4ae3a
Mashiyat <mashiyat.rahman@g.bracu.ac.bd>
2026-08-17 00:40:55 +0600

    Complete emergency SOS donor matching feature with GPS and donor seed data

--- database/seeders/DonorSeeder.php
+++ database/seeders/DonorSeeder.php
@@ -2,22 +2,142 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Seeder;
 use App\Models\Donor;
+use App\Models\User;
+
 
 class DonorSeeder extends Seeder
 {
     public function run(): void
     {
-        Donor::create([
-            'user_id' => 1,
-            'blood_group' => 'O+',
-            'phone' => '01700000001',
-            'latitude' => 23.8103,
-            'longitude' => 90.4125,
-            'is_willing' => true,
-            'is_available' => true,
-            'is_verified' => true,
-        ]);
+
+
+        $karim = User::firstOrCreate(
+            [
+                'email' => 'karim.donor@gmail.com'
+            ],
+            [
+                'name' => 'Karim Donor',
+                'password' => bcrypt('password'),
+                'role' => 'donor'
+            ]
+        );
+
+
+        Donor::firstOrCreate(
+
+            [
+                'user_id' => $karim->id
+            ],
+
+            [
+
+                'blood_group' => 'B+',
+
+                'phone' => '01722222222',
+
+                'latitude' => 23.8103,
+
+                'longitude' => 90.4125,
+
+                'is_willing' => true,
+
+                'is_available' => true,
+
+                'is_verified' => true,
+
+            ]
+
+        );
+
+
+
+
+
+        $sakib = User::firstOrCreate(
+            [
+                'email' => 'sakib.donor@gmail.com'
+            ],
+            [
+                'name' => 'Sakib Donor',
+                'password' => bcrypt('password'),
+                'role' => 'donor'
+            ]
+        );
+
+
+
+        Donor::firstOrCreate(
+
+            [
+                'user_id' => $sakib->id
+            ],
+
+            [
+
+                'blood_group' => 'O-',
+
+                'phone' => '01733333333',
+
+                'latitude' => 23.8150,
+
+                'longitude' => 90.4200,
+
+                'is_willing' => true,
+
+                'is_available' => true,
+
+                'is_verified' => true,
+
+            ]
+
+        );
+
+
+
+
+
+        $rahim = User::firstOrCreate(
+            [
+                'email' => 'rahim.donor@gmail.com'
+            ],
+            [
+                'name' => 'Rahim Donor',
+                'password' => bcrypt('password'),
+                'role' => 'donor'
+            ]
+        );
+
+
+
+        Donor::firstOrCreate(
+
+            [
+                'user_id' => $rahim->id
+            ],
+
+            [
+
+                'blood_group' => 'A+',
+
+                'phone' => '01711111111',
+
+                'latitude' => 23.8200,
+
+                'longitude' => 90.4300,
+
+                'is_willing' => true,
+
+                'is_available' => true,
+
+                'is_verified' => true,
+
+            ]
+
+        );
+
+
     }
 }
