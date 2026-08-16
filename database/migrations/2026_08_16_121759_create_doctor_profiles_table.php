a6b707586fb243a0927978d4289b6dfdc9d446ce
Mashiyat <mashiyat.rahman@g.bracu.ac.bd>
2026-08-16 18:52:58 +0600

    Add doctor profile module

---
+++ database/migrations/2026_08_16_121759_create_doctor_profiles_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_profiles', function (Blueprint $table) {
+
+            $table->id();
+
+            // Connect with users table
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            $table->string('license_number')
+                  ->unique();
+
+            $table->string('specialization')
+                  ->nullable();
+
+            $table->string('hospital')
+                  ->nullable();
+
+            $table->string('phone')
+                  ->nullable();
+
+            $table->timestamps();
+
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor_profiles');
+    }
+};
